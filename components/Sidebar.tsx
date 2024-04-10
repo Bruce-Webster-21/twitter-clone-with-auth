c05868e4fbb7f49131219b9deb458112462d1900
Bruce Webster <brucewebster222@gmail.com>
2024-04-10 15:46:17 +0200

    Created Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,13 @@
 import { BsBellFill, BsHouseFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
 
 import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import SidebarTweetBtn from "./SidebarTweetBtn";
 
 const Sidebar = () => {
-  const item = [
+  const items = [
     {
       label: "Home",
       href: "/",
@@ -26,6 +29,16 @@ const Sidebar = () => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+          <SidebarTweetBtn />
         </div>
       </div>
     </div>
---
+++ components/SidebarTweetBtn.tsx
@@ -0,0 +1,21 @@
+import { FaFeather } from "react-icons/fa";
+
+import { useRouter } from "next/router";
+
+const SidebarTweetBtn = () => {
+  const router = useRouter();
+
+  return (
+    <div
+      onClick={() => {
+        router.push("/");
+      }}
+    >
+      <div className="mt-6 lg:hidden rounded-full h-14 w-14 p-4 flex items-center justify-center bg-sky-500 hover:bg-opacity-80 transition cursor-pointer">
+        <FaFeather size={28} color="white" />
+      </div>
+    </div>
+  );
+};
+
+export default SidebarTweetBtn;
