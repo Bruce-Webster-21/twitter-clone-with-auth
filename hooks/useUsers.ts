5163c41d3d281fc2602becd631d7c6ac7c9d09df
Bruce <brucewebster222@gmail.com>
2024-05-22 14:10:49 +0200

    Created Follower bar user

---
+++ hooks/useUsers.ts
@@ -0,0 +1,16 @@
+// https://swr.vercel.app/
+import useSWR from "swr";
+import fetcher from "@/libs/fetcher";
+
+const useUsers = () => {
+  const { data, error, isLoading, mutate } = useSWR("/api/users", fetcher);
+
+  return {
+    data,
+    error,
+    isLoading,
+    mutate,
+  };
+};
+
+export default useUsers;
