a30dcd4a1fb8391a6f29999bfade838b244a02e3
Bruce <brucewebster222@gmail.com>
2024-06-26 16:04:47 -0400

    Created editModal hook and api

---
+++ hooks/useEditModal.ts
@@ -0,0 +1,18 @@
+// https://docs.pmnd.rs/zustand/getting-started/introduction
+// Explanation of Zustand
+
+import { create } from "zustand";
+
+interface EditModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useEditModal = create<EditModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useEditModal;
