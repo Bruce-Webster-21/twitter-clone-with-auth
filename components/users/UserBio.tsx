7fa528a93ef34155f2ce924830731fff5551e150
Bruce <brucewebster222@gmail.com>
2024-06-26 14:29:47 -0400

    Added Following and followers to UserBio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -45,6 +45,18 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
             <p>Joined {createdAt}</p>
           </div>
         </div>
+        <div className="flex flex-row items-center mt-4 gap-6">
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">
+              {fetchedUser?.followingIds?.length || 0}
+            </p>
+            <p className="text-neutral-500">Following</p>
+          </div>
+          <div className="flex flex-row items-center gap-1">
+            <p className="text-white">{fetchedUser?.followersCount || 0}</p>
+            <p className="text-neutral-500">Followers</p>
+          </div>
+        </div>
       </div>
     </div>
   );
