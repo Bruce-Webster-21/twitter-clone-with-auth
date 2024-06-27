e2c2e5fba6fe298e114ee627e5f6f2335b172a73
Bruce <brucewebster222@gmail.com>
2024-06-27 17:21:47 -0400

    Added edit modal to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import EditModal from "@/components/modals/EditModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -14,6 +15,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Toaster />
         <RegisterModal />
         <LoginModal />
+        <EditModal />
         <Layout>
           <Component {...pageProps} />
         </Layout>
